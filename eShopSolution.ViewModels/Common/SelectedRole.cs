653c661c8c2f71bd264d2122660281aa5b37f788
duckkoder <trungduc02042004@gmail.com>
2024-10-28 19:10:29 +0700

    add css table for RoleAssign Feature

--- eShopSolution.ViewModels/Common/SelectedRole.cs
+++ eShopSolution.ViewModels/Common/SelectedRole.cs
@@ -11,6 +11,7 @@ namespace eShopSolution.ViewModels.Common
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
         public bool IsSelected { get; set; }
     }
 }
